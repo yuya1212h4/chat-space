9230108e645e4c049d0fe01dad188f307851aee7
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-10-23 12:41:16 +0900

    add columns of  groups tables

--- db/migrate/20161023033352_create_groups.rb
+++ db/migrate/20161023033352_create_groups.rb
@@ -1,6 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
+      t.string        :name, null: false
+      t.references    :user
 
       t.timestamps
     end
