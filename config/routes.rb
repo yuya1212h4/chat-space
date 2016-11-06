11bae55b83b9d57ad597b170179592e4c9763e7b
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-06 18:43:23 +0900

    fix routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :groups, only: [:index, :create]
+  resources :groups, only: [:index]
   resources :messages, only: [:create]
 end
