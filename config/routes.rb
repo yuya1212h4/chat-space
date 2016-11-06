78d3280d5e8972c1dcac36e436a7548ff689b1da
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-06 19:39:28 +0900

    routing nest

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :groups, only: :index
-  resources :messages, only: :create
+  resources :groups, only: :index do
+    resources :messages, only: :create
+  end
 end
