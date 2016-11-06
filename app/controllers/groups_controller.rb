77665e7f7239b8e4a67df549ec168c8b21ef3f5a
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-07 01:32:14 +0900

    add group_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,5 +2,8 @@ class GroupsController < ApplicationController
   def index
     @group = Group.new
     @message = Message.new
+    # @messages = Message.group(:message_id)
+    @messages = Message.all
+    # binding.pry
   end
 end
