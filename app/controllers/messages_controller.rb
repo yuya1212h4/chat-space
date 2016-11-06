c6a156bd9faa28de8a1019c1ce862415167723af
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-07 01:31:58 +0900

    add message_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,12 @@
 class MessagesController < ApplicationController
   def create
+    Message.create(message_params)
+    redirect_to groups_path
+  end
+
+
+  private
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
