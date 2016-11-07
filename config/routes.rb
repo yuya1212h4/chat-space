a1161e395ef1b4837c4f1ad65a876fc4cd1a6bcf
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-07 10:07:01 +0900

    add description controller on routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   root 'groups#index'
 
   resources :groups, only: :index do
