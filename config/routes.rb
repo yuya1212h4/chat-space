2addd84faa418d6d679d63932cfa885d45252342
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-09 10:17:58 +0900

    create users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'groups#index'
 
+  resources :user
   resources :groups, only: :index do
     resources :messages, only: :create
   end
