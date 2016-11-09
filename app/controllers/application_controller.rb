072e5df1390515025ddb99e11ada9687e0d319bd
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-09 16:08:32 +0900

    decide path on application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+   add_flash_types :success, :info, :warning, :danger
+
   def after_sign_in_path_for(resource)
-    groups_path
+    root_path
   end
 
   def after_sign_out_path_for(resource)
-    groups_path
+    new_user_session_path
   end
 
   def configure_permitted_parameters
