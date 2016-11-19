d09de1e9b647635765070cb3c7d143376a45b22e
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-19 13:37:32 +0900

    add group_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,44 @@
 class GroupsController < ApplicationController
   def index
-    @group = Group.new
+    # @group = Group.new
     @message = Message.new
-    # @messages = Message.group(:message_id)
+    # # @messages = Message.group(:message_id)
     @messages = Message.all
-    # binding.pry
+    # # binding.pry
   end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    # binding.pry
+    # @group = Group.find(params[:group_id])
+    # @message = Message.new
+
+    @group = Group.new
+    @group.users.build
+  end
+
+  def create
+    binding.pry
+    # Group.create(group_params)
+    # group = Group.last.id
+    Group.create(group_params)
+    # Group.create(users_params)
+    # params.require(:groups).where()
+    binding.pry
+    redirect_to root_path
+  end
+
+  private
+  def group_params
+    # params.require(:group).permit(:name, :user_id).merge(group_id: Group.last.id, user_id: current_user.id)
+    params.require(:group).permit(:name, users_attributes: [:user_id])
+    # params.require(:post).permit(:user_id)
+  end
+  #
+  # def users_params
+  #   params.require(:users).permit(:user_id)
+  # end
 end
