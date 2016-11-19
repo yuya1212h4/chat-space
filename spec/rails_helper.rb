6da02ef6fb2fec0050f237b7d490cb146c9c6edf
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-19 19:44:38 +0900

    add rails-helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'rails-controller-testing'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -47,4 +48,12 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  require 'rails-controller-testing'
+  [:controller, :view, :request].each do |type|
+   config.include ::Rails::Controller::Testing::TestProcess, :type => type
+   config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
+   config.include ::Rails::Controller::Testing::Integration, :type => type
+  end
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 end
