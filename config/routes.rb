31b56aff6d9a69c057fa708e9962c6122a3fb0ed
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-20 17:18:56 +0900

    fix roures.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'groups#index'
 
-  resources :groups, only: [:edit, :new, :create, :update] do
+  resources :groups, except: [:index, :show, :destroy], shallow: true do
     resources :messages, only: [:new,:create]
   end
 end
