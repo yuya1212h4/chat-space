9a615357dcff30a8110b9a589a11ca55ee417de2
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-20 11:09:04 +0900

    fix gorups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,27 +18,38 @@ class GroupsController < ApplicationController
 
     @group = Group.new
     @group.users.build
+    # binding.pry
   end
 
   def create
-    binding.pry
     # Group.create(group_params)
     # group = Group.last.id
-    Group.create(group_params)
+    # binding.pry
+    group = Group.create(group_params)
+    # binding.pry
+
+    # binding.pry
+    # group_ids = Group.last.id
+    # users_params.each do |n|
+    #   # Group.create(gorup_id: )
+    # end
+    #
+    binding.pry
     # Group.create(users_params)
     # params.require(:groups).where()
-    binding.pry
     redirect_to root_path
   end
 
   private
   def group_params
     # params.require(:group).permit(:name, :user_id).merge(group_id: Group.last.id, user_id: current_user.id)
-    params.require(:group).permit(:name, users_attributes: [:user_id])
+    params.require(:group).permit(:name, {user_ids: []})
+    # params.require(:group).permit(:name)
     # params.require(:post).permit(:user_id)
   end
-  #
-  # def users_params
-  #   params.require(:users).permit(:user_id)
-  # end
+
+  def users_params
+    # params.require(:users)[:user_id]
+    params.require(:users).permit(user_id: [])
+  end
 end
