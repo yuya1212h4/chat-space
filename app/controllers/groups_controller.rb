cbaf6c0c2769ed55940996319d9bd141e685572a
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-20 15:29:04 +0900

    edit groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,55 +1,35 @@
 class GroupsController < ApplicationController
+  protect_from_forgery :except => ["edit"]
+
   def index
-    # @group = Group.new
-    @message = Message.new
-    # # @messages = Message.group(:message_id)
-    @messages = Message.all
-    # # binding.pry
+  end
+
+  def new
+    @group = Group.new
+    @group.users.build
+  end
+
+  def create
+    group = Group.create(group_params)
+    redirect_to root_path
   end
 
   def edit
     @group = Group.find(params[:id])
   end
 
-  def new
-    # binding.pry
-    # @group = Group.find(params[:group_id])
-    # @message = Message.new
-
-    @group = Group.new
-    @group.users.build
-    # binding.pry
-  end
-
-  def create
-    # Group.create(group_params)
-    # group = Group.last.id
-    # binding.pry
-    group = Group.create(group_params)
-    # binding.pry
-
-    # binding.pry
-    # group_ids = Group.last.id
-    # users_params.each do |n|
-    #   # Group.create(gorup_id: )
-    # end
-    #
-    binding.pry
-    # Group.create(users_params)
-    # params.require(:groups).where()
-    redirect_to root_path
+  def update
+    group = Group.find(params[:id])
+    group.update(group_params)
+    redirect_to new_group_message_path(params[:id])
   end
 
   private
   def group_params
-    # params.require(:group).permit(:name, :user_id).merge(group_id: Group.last.id, user_id: current_user.id)
     params.require(:group).permit(:name, {user_ids: []})
-    # params.require(:group).permit(:name)
-    # params.require(:post).permit(:user_id)
   end
 
   def users_params
-    # params.require(:users)[:user_id]
     params.require(:users).permit(user_id: [])
   end
 end
