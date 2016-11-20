b8794440ce93447e42377d383679a9f8e5847712
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-20 20:21:51 +0900

    fix controllers/messges

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,15 +2,30 @@ class MessagesController < ApplicationController
   def new
     @message = Message.new
     @messages = Message.all
+    # @messages = Group.messages.all
   end
 
   def create
-    Message.create(message_params)
-    redirect_to new_group_message_path(params[:group_id])
+    # binding.pry
+    message = Message.new(message_params)
+    if message.present?
+      message.save
+        redirect_to new_group_message_path(params[:group_id]), notice: "メッセージの投稿が完了しました。"
+      else
+        redirect_to new_group_message_path(params[:group_id]), notice: "メッセージの送信に失敗しました。"
+    end
   end
 
   private
+
   def message_params
     params.require(:message).permit(:body)
   end
+  # def message_params
+  #   params.permit(:group_id, {message: :body})
+  # end
+
+  # def group_params
+  #   params.permit(:group_id)
+  # end
 end
