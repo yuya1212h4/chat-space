24f80c96157816e3fff6bc6f4c4bd2d850fd12e2
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-20 11:35:02 +0900

    rspec group_controller

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 describe GroupsController, type: :controller do
 
   describe 'GET #index' do
-
     it "render template index" do
       get :index
-      expect(response).to render_template("index")
+      expect(response).to render_template root_path
+      # expect(response.status).to eq 200
       # response.should render_template(:index)
     end
   end
