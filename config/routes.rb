d9ff879d280e8a1f1cb8b355154c6d00639d2300
Yuya Tabata <yuya1212h4.066@gmail.com>
2016-11-20 15:26:18 +0900

    edit rotues.rb add update action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'groups#index'
 
-  resources :groups, only: [:index, :edit, :new, :create] do
-    resources :messages, only: :create
+  resources :groups, only: [:edit, :new, :create, :update] do
+    resources :messages, only: [:new,:create]
   end
 end
