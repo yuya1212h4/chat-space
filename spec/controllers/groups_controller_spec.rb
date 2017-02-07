295b2a88428760c0d73a492692d5eb8473e4d1d5
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-02-07 19:15:26 +0900

    delete new lines

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -12,26 +12,22 @@ describe GroupsController, type: :controller do
       expect(response).to render_template root_path
     end
   end
-
   describe 'GET #new' do
     it "render the :new template" do
       get :new
       expect(response).to render_template :new
     end
   end
-
   describe 'GET #edit' do
     it "render the :edit template" do
       group = create(:group)
       get :edit, params: { id: group }
       expect(response).to render_template :edit
     end
-
     it "assigns the requested contanct to @group" do
       group = create(:group)
       get :edit, params: { id: group }
       expect(assigns(:group)).to eq group
     end
   end
-
 end
--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -15,21 +15,17 @@ describe MessagesController, type: :controller do
       expect(response).to render_template :new
     end
   end
-
   context "with valid attributes" do
     it "saves the new message in the database" do
       expect{ create :message}.to change{ Message.count }.by(1)
     end
-
     it 'redirects to messages#create' do
       post :create, params: { message: attributes_for(:message), group_id: group.id }
       expect(response).to redirect_to new_group_message_path
     end
-
     it 'render the :show template' do
       post :create, params: { message: attributes_for(:message, body: ""), group_id: group.id }
       expect(response).to render_template :"messages/new"
     end
   end
-  
  end
