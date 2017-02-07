40c90f7f5ef13e730818c75e14ed546eb4eb563f
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-02-07 12:28:08 +0900

    add spec/controllers/messages_contoller_spec.rb

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,10 +1,35 @@
 require 'rails_helper'
-  describe MessagesController do
-    describe 'GET #index' do
-      it "renders the :index templete" do
-        post :create
-        expect(response).to redirect_to root_path
-      end
+describe MessagesController, type: :controller do
+
+  let(:group){ create(:group) }
+  let(:message){ create(:message, group_id: group.id, user_id: user.id) }
+
+  before do
+   user = create(:user)
+   sign_in user
   end
 
-end
+  describe 'GET #new' do
+    it "renders the :new template" do
+      get :new, params: {group_id: group.id}
+      expect(response).to render_template :new
+    end
+  end
+
+  context "with valid attributes" do
+    it "saves the new message in the database" do
+      expect{ create :message}.to change{ Message.count }.by(1)
+    end
+
+    it 'redirects to messages#create' do
+      post :create, params: { message: attributes_for(:message), group_id: group.id }
+      expect(response).to redirect_to new_group_message_path
+    end
+
+    it 'render the :show template' do
+      post :create, params: { message: attributes_for(:message, body: ""), group_id: group.id }
+      expect(response).to render_template :"messages/new"
+    end
+  end
+  
+ end
