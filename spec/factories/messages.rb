9efe367c5ee3db8d38d76998e3bc3170a074c1a8
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-02-07 19:14:53 +0900

    change string class to integer class

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
-
   factory :message do
     body          "text"
     image         ""
-    user_id       "1"
-    group_id      "1"
+    user_id       1
+    group_id      1
   end
 end
