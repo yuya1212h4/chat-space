3972d221529f5bbc7e4878e85f1e37ea9f58005e
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-02-07 19:11:43 +0900

    delete new lines

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,17 +1,14 @@
 require 'rails_helper'
 describe Message, type: :model do
   describe '#create' do
-
     it "is effective with a message" do
       message = build(:message)
       expect(message).to be_valid
     end
-
     it "is invalid without a message" do
       message = build(:message, body: "")
       message.valid?
       expect(message.errors[:body]).to include("を入力してください")
     end
-
   end
 end
