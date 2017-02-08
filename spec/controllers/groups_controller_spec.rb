67c16b669dae54e8d2a24b956fadaf1613c01f9c
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-02-09 07:38:52 +0900

    add group bailable

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
+
 describe GroupsController, type: :controller do
+  let(:group){ create(:group) }
 
   before do
    user = create(:user)
@@ -12,20 +14,21 @@ describe GroupsController, type: :controller do
       expect(response).to render_template root_path
     end
   end
+
   describe 'GET #new' do
     it "render the :new template" do
       get :new
       expect(response).to render_template :new
     end
   end
+
   describe 'GET #edit' do
     it "render the :edit template" do
-      group = create(:group)
       get :edit, params: { id: group }
       expect(response).to render_template :edit
     end
+
     it "assigns the requested contanct to @group" do
-      group = create(:group)
       get :edit, params: { id: group }
       expect(assigns(:group)).to eq group
     end
