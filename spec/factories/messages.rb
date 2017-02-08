27b68570558e3b9a72367f15ec0b1af63cc2c5d6
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-02-09 07:37:09 +0900

    add image faker

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     body          "text"
-    image         ""
+    image         { Faker::Avatar.image }
     user_id       1
     group_id      1
   end
