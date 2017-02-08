34f0dea642b3ae74bd75c17c93667784eeca14eb
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-02-09 07:37:20 +0900

    add blank

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
-describe MessagesController, type: :controller do
 
+describe MessagesController, type: :controller do
   let(:group){ create(:group) }
   let(:message){ create(:message, group_id: group.id, user_id: user.id) }
 
@@ -11,20 +11,23 @@ describe MessagesController, type: :controller do
 
   describe 'GET #new' do
     it "renders the :new template" do
-      get :new, params: {group_id: group.id}
+      get :new, params: { group_id: group.id }
       expect(response).to render_template :new
     end
   end
+
   context "with valid attributes" do
     it "saves the new message in the database" do
-      expect{ create :message}.to change{ Message.count }.by(1)
+      expect{ create :message }.to change{ Message.count }.by(1)
     end
+
     it 'redirects to messages#create' do
       post :create, params: { message: attributes_for(:message), group_id: group.id }
       expect(response).to redirect_to new_group_message_path
     end
+
     it 'render the :show template' do
-      post :create, params: { message: attributes_for(:message, body: ""), group_id: group.id }
+      post :create, params: { message: attributes_for(:message, body: nil), group_id: group.id }
       expect(response).to render_template :"messages/new"
     end
   end
