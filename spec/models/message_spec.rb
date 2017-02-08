0a8f0cab7bbad92b04d36ab7cb36bd355d0bf9c5
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-02-09 07:36:27 +0900

    fix context

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
+
 describe Message, type: :model do
-  describe '#create' do
+  context "with valid attributes" do
     it "is effective with a message" do
       message = build(:message)
       expect(message).to be_valid
     end
+  end
+
+  context "with invalid attributes" do
     it "is invalid without a message" do
       message = build(:message, body: "")
       message.valid?
