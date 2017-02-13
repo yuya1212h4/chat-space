c9ab9b2c2d307aa2f46b0d2d571991d1a4e9a38f
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-02-13 15:20:13 +0900

    change message_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,10 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to new_group_message_path(@group), notice: "メッセージの投稿が完了しました。"
+      respond_to do |format|
+        format.html { redirect_to new_group_message_path(@group), notice: "メッセージの投稿が完了しました。" }
+        format.json { render json: { name: @message.user.name, created_at: @message.created_at, body: @message.body } }
+      end
     else
       flash.now[:alert] = "メッセージの送信に失敗しました。"
       render :new
@@ -18,7 +21,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:body).merge(group_id: params[:group_id])
+    params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 
   def set_group
