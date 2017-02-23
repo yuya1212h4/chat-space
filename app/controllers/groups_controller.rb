0b920a93749482a0614026f157e78a5857c95a17
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-02-23 18:44:45 +0900

    create serch_action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,6 +9,13 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def search
+    @users = User.where('name LIKE(?)',"%#{search_params[:name]}%")
+    respond_to do |format|
+      format.json
+    end
+  end
+
   def create
     @group = Group.new(group_params)
     if @group.save
@@ -39,5 +46,9 @@ class GroupsController < ApplicationController
 
   def set_group
     @group = Group.find(params[:id])
+  end
+
+  def search_params
+    params.permit(:name)
   end
 end
