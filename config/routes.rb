56e5558906795beeeaf94f0555159da3ce9836d6
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-02-23 18:42:16 +0900

    define search action on groups_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'groups#index'
+  get 'search' => 'groups#search'
 
   resources :groups, only: [:new, :create, :edit, :update], shallow: true do
     resources :messages, only: [:new, :create]
