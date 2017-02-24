95c6414700897d9bbe1a31fa241084ce400f074b
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-02-24 09:31:48 +0900

    fix users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,9 @@
 class UsersController < ApplicationController
 
   def search
-    @users = User.where('name LIKE(?)',"%#{search_params[:name]}%")
+    @users = User.where('name LIKE(?)',"%#{params[:name]}%")
     respond_to do |format|
       format.json
     end
-  end
-
-  private
-
-  def search_params
-    params.permit(:name)
   end
 end
