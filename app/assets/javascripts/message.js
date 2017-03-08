2f68c379f4480a6e286d6028a6cf515f46e5f847
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-03-08 09:19:55 +0900

    fix last_message_id

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -55,7 +55,6 @@ $(document).on('turbolinks:load', function() {
 
   var autoReload = setInterval(function() {
     if (window.location.href.match(/\/groups\/\d+\/messages\/new/)){
-      var last_message_id = $('.chat-message').last().data('message-id');
       var last_message_id = $('.chat-message').last().data('message-id') || 0;
       $.ajax({
         type: 'GET',
