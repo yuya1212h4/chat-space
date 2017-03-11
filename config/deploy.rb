6db3031aeccac279568d3f28fd38c9a0c42eac54
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-03-11 23:21:22 +0900

    configurate config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,73 @@
 # config valid only for current version of Capistrano
-lock "3.8.0"
+# capistrano のバージョン固定
+lock "3.4.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+# デプロイするアプリケーション名
+set :application, "chat-space"
+
+# cloneするgitのリポジトリ
+set :repo_url, "git@github.com:yuya1212h4/chat-space.git"
+
+# deployするブランチ。デフォルトはmasterなのでなくても可。
+set :branch, 'master'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/chat-space"
+
+# シンボリックリンクをはるファイル。(※後述)
+set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
+
+# シンボリックリンクをはるフォルダ。(※後述)
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# 保持するバージョンの個数(※後述)
+set :keep_releases, 5
+
+# rubyのバージョン
+set :rbenv_ruby, '2.1.3'
+
+#出力するログのレベル。
+set :log_level, :debug
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
+  end
+end
+
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
