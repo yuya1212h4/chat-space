1d5a6c6eb885db4d77ce7e9d3ca8d7dd41356ffc
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-09-02 14:23:15 +0900

    fix application_controller.rb
    デフォルトでroot_pathに飛ぶので、指定の必要がないため。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,18 +7,6 @@ class ApplicationController < ActionController::Base
 
    add_flash_types :success, :info, :warning, :danger
 
-  # def after_sign_up_path_for(resource)
-  #  root_path
-  # end
-  #
-  # def after_sign_in_path_for(resource)
-  #   root_path
-  # end
-  #
-  # def after_sign_out_path_for(resource)
-  #   new_user_session_path
-  # end
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
