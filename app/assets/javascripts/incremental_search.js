616714595890b8aecf6729464f56ccb1c851984b
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-09-02 15:39:28 +0900

    fix incremental_search.js
    インクリメンタルサーチにおけるファイルのメソッドの切り出し
    バックスペースを押したときに、全てのユーザーを表示しないようにするためのバグの修正

--- app/assets/javascripts/incremental_search.js
+++ app/assets/javascripts/incremental_search.js
@@ -1,56 +1,4 @@
 $(document).on('turbolinks:load', function() {
-  function bulidSearchUserHTML(user) {
-    var html = `
-      <div class = "chat-group-user clearfix">
-        <p class = "chat-group-user__name">
-          ${user.name}
-        </p>
-        <a class ="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}"  data-user-name="${user.name}" >
-        追加
-        </a>
-      </div>
-    `;
-    return html;
-  }
-
-  function buildAddUserHTML(id, name) {
-    var html = `
-      <div class = "chat-group-user clearfix" id="chat-group-user-${id}">
-        <p class = "chat-group-user__name">
-          ${name}
-        </p>
-        <a class="user-search-remove chat-group-user__btn  chat-group-user__btn--remove" data-user-id="${id}">
-          削除
-        </a>
-        <input value="${id}", name='group[user_ids][]' type="hidden" />
-      </div>
-    `
-    return html;
-  }
-
-  function searchUsers() {
-    var name = $(this).val();
-    $.ajax({
-      type: 'GET',
-      url: '/users/search',
-      data: {
-        name: name,
-      },
-      dataType: 'json'
-    })
-    .done(function(user) {
-      var insertHTML = '';
-
-      user.forEach(function(user){
-        insertHTML += bulidSearchUserHTML(user);
-      });
-
-      $('#user-search-result').html(insertHTML);
-    })
-    .fail(function(){
-      alert('error');
-    });
-  }
 
   $('#inc_search').on('keyup', searchUsers);
 
@@ -59,15 +7,72 @@ $(document).on('turbolinks:load', function() {
     var name = $(this).data('user-name');
     var user = $(this).parent();
     var insertHTML = buildAddUserHTML(id, name);
-
     $('#chat-group-users').append(insertHTML);
-
-    user.remove();
+    user.remove();  // 追加ボタンを押した後に、そのユーザーを削除
   });
 
   $('#chat-group-users').on('click', '.user-search-remove', function(){
     var id = $(this).data('user-id');
-
-    $(`#chat-group-user-${id}`).remove();
+    $(`#chat-group-user-${id}`).remove(); // 追加後のuserの削除ボタン
   })
+
 });
+
+function bulidSearchUserHTML(user) {
+  var html = `
+    <div class = "chat-group-user chat-remove clearfix">
+      <p class = "chat-group-user__name">
+        ${user.name}
+      </p>
+      <a class ="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}"  data-user-name="${user.name}" >
+      追加
+      </a>
+    </div>
+  `;
+  return html;
+}
+
+function buildAddUserHTML(id, name) {
+  var html = `
+    <div class = "chat-group-user clearfix" id="chat-group-user-${id}">
+      <p class = "chat-group-user__name">
+        ${name}
+      </p>
+      <a class="user-search-remove chat-group-user__btn  chat-group-user__btn--remove" data-user-id="${id}">
+        削除
+      </a>
+      <input value="${id}", name='group[user_ids][]' type="hidden" />
+    </div>
+  `
+  return html;
+}
+
+function searchUsers() {
+  var inputname = $(this).val();
+  var preInput = "";
+  if(inputname !== preInput){
+  $.ajax({
+    type: 'GET',
+    url: '/users/search',
+    data: {
+      name: inputname,
+    },
+    dataType: 'json'
+  })
+  .done(function(user) {
+    var insertHTML = '';
+
+    user.forEach(function(user){
+      insertHTML += bulidSearchUserHTML(user);
+    });
+
+    $('#user-search-result').html(insertHTML);
+  })
+  .fail(function(){
+    alert('error');
+  });
+  preIput = inputname;
+  } else {
+    $(".chat-remove").remove(); // 検索を終えた後に、ユーザーをそのまま表示にして置かないようにするため
+  }
+}
