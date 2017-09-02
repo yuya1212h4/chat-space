dab46272dddc0eb5fadc296d29459c099a828e43
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-09-02 13:58:12 +0900

    fix message.js
    jsでのフラッシュメッセージの追加
    メソッドの切り出し

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,55 +1,39 @@
 $(document).on('turbolinks:load', function() {
-  function buildHTML(message) {
-    var image = (message.image) ? `<img src = ${message.image}>` : "";
-
-    var html = `
-      <li class = "chat-message" data-message-id="${message.id}">
-        <div class = "chat-message__header clearfix">
-          <div class = "chat-message__name">
-            ${message.name}
-          </div>
-          <div class = "chat-message__time">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class = "chat-message__body">
-          ${message.body}
-        </div>
-          ${image}
-      </li>
-      `;
-    return html;
-  }
-
-  function scrollBottom(){
-    $('.chat-body').animate({
-      scrollTop: $('.chat-messages').height()
-    });
-  }
 
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    var textField = $('#message_body');
-    var message = textField.val();
     var formData = new FormData($(this).get(0));
-    $.ajax({
-      type: 'POST',
-      url: '../messages',
-      data: formData,
-      processData: false,
-      contentType: false,
-      dataType: 'json'
-    })
-    .done(function(message) {
-      var html = buildHTML(message);
-      $('.chat-messages').append(html);
-      textField.val('');
+    var flash_status = '';
+    $('.flash-messages__notice').remove();
+    $('.flash-messages__alert').remove();
 
-      scrollBottom();
-    })
-    .fail(function() {
-      alert('error');
-    });
+    if (formData.get('message[body]') != ""){
+      $.ajax({
+        type: 'POST',
+        url: '../messages',
+        data: formData,
+        processData: false,
+        contentType: false,
+        dataType: 'json'
+      })
+      .done(function(message) {
+        var html = buildHTML(message);
+        $('.chat-messages').append(html);
+        $('#message_body').val('');
+
+        scrollBottom();
+        flash_status = 'notice';
+        var flash = buildflashmessage(flash_status);
+        $(".flash-messages").prepend(flash);
+      })
+      .fail(function() {
+        alert('error');
+      });
+    } else {
+      flash_status = 'alert';
+      var flash = buildflashmessage(flash_status);
+      $('.flash-messages').prepend(flash);
+    }
     return false;
   });
 
@@ -76,5 +60,45 @@ $(document).on('turbolinks:load', function() {
       });
     } else {
       clearInterval(autoReload);
-    }}, 10000);
+    }}, 100000);
 });
+
+
+function buildHTML(message) {
+  var image = (message.image) ? `<img src = ${message.image}>` : "";
+
+  var html = `
+    <li class = "chat-message" data-message-id="${message.id}">
+      <div class = "chat-message__header clearfix">
+        <div class = "chat-message__name">
+          ${message.name}
+        </div>
+        <div class = "chat-message__time">
+          ${message.created_at}
+        </div>
+      </div>
+      <div class = "chat-message__body">
+        ${message.body}
+      </div>
+        ${image}
+    </li>
+    `;
+  return html;
+}
+
+function buildflashmessage(status) {
+  (status == 'notice') ? message = "投稿完了" : message = "投稿失敗";
+
+  var flash = `
+      <div class = "flash-messages__${status}">
+        ${ message }
+      </div>
+    `;
+  return flash;
+}
+
+function scrollBottom(){
+  $('.chat-body').animate({
+    scrollTop: $('.chat-messages').height()
+  });
+}
