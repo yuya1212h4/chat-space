87a58847e21885feba863dbec9147e0419732350
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-09-03 11:45:40 +0900

    fix message.js
    フォームに画像が投稿されたときも、ajaxを動作出来るように修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -7,7 +7,7 @@ $(document).on('turbolinks:load', function() {
     $('.flash-messages__notice').remove();
     $('.flash-messages__alert').remove();
 
-    if (formData.get('message[body]') != ""){
+    if ((formData.get('message[body]') || formData.get('message[image]')) != ""){
       $.ajax({
         type: 'POST',
         url: '../messages',
