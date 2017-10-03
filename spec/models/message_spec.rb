e0a12febf0cf84d3e95068502aaceeb969d88b04
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-10-03 17:56:40 +0900

    fix models/message_spec.rb
    bodyが無くても、imageがあれば投稿出来るようにしたため、それに対応出来るようにテストの修正

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -12,7 +12,9 @@ describe Message, type: :model do
     it "is invalid without a message" do
       message = build(:message, body: "")
       message.valid?
-      expect(message.errors[:body]).to include("を入力してください")
+      expect(message).to be_valid
+    end
+
     end
   end
 end
