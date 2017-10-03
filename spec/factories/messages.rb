db228d9b0aecbaa0970afd0deac56df200c03d64
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-10-03 19:18:08 +0900

    update image upload on rspec

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,10 @@
+#include ActionDispatch::TestProcess
+# ここにincludeを書くか、rails_helperに書くかのどちらか。
+
 FactoryGirl.define do
   factory :message do
     body          "text"
-    image         { Faker::Avatar.image }
+    image         { fixture_file_upload("spec/fixtures/img/sample.png", 'image/png') }
     user_id       1
     group_id      1
   end
