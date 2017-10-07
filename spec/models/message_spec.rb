da59cfa143f8f1d874d9b1f1e8779c1beeaf7d52
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-10-07 09:14:44 +0900

    rspec models/messages_spec.rb

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -21,10 +21,25 @@ describe Message, type: :model do
       expect(message).to be_valid
     end
 
+  end
+
+  context "without valid attributes" do
     it "is invalid without a body and image" do
       message = build(:message, body: "", image: "")
       message.valid?
       expect(message.errors[:body_or_image]).to include("を入力してください")
+    end
+
+    it "is invalid without group_id" do
+      message = build(:message, group_id: nil)
+      message.valid?
+      expect{ message.save }.to raise_error(ActiveRecord::StatementInvalid)
+    end
+
+    it "is invalid without user_id" do
+      message = build(:message, user_id: nil)
+      message.valid?
+      expect{ message.save }.to raise_error(ActiveRecord::StatementInvalid)
     end
   end
 end
