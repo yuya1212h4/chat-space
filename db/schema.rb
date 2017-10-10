3c210ffb0c37117b7e2f3b1ee14feb7fae10e0a6
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-10-10 12:38:53 +0900

    add migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023050303) do
+ActiveRecord::Schema.define(version: 20171003104944) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(version: 20161023050303) do
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "body",       limit: 65535
     t.string   "image"
-    t.integer  "user_id"
-    t.integer  "group_id"
+    t.integer  "user_id",                  null: false
+    t.integer  "group_id",                 null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
