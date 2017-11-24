e45bd33d1f2c5ca16ff0aa082b92914dad7f5b3a
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-11-24 12:32:18 +0900

    create show_last_message on models/group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,13 @@ class Group < ApplicationRecord
   has_many :group_users
   has_many :messages
 
-  accepts_nested_attributes_for :users
-
   validates :name, presence: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.body? ? last_message.body : '画像が投稿されてされています。'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
